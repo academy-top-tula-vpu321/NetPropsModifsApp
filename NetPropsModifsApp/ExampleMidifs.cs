043e1a8ea7dedf1ba227179597f85d94fbf24111
maxefimov <efimov.top.academy@gmail.com>
2024-02-18 11:05:12 +0300

    Добавьте файлы проекта.

---
+++ NetPropsModifsApp/ExampleMidifs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetPropsModifsApp
+{
+
+    // виден только внутри файла
+    file class ClassFile { }
+
+    // public для этой сборки
+    // private для внешних сборок
+    internal class ClassInternal { }
+
+    public class ClassPublic { }
+
+    class ModifiersExample
+    {
+        // виден только внутри класса или структуры
+        private int number;
+        int amount;
+
+        // виден только внутри класса или структуры,
+        // а также в производных классах
+        // в рамках одной сборки
+        private protected float x;
+
+        protected internal string name;
+    }
+}
---
+++ NetPropsModifsApp/Program.cs
@@ -0,0 +1,97 @@
+﻿
+User user = new User()
+{
+    Name = "Bobby"
+};
+
+user.Name = "Bob";
+user.Age = 26;
+
+Console.WriteLine(user.Name);
+
+User tom = new User("Tommy", 32) { Name = "Tommy" };
+//tom.Type = "admin"; // ERROR
+//Console.WriteLine(tom.Type);
+
+
+
+class User
+{
+    string type;
+    int age;
+
+    public User()
+    {
+        //this.Name = name;
+        //this.age = age;
+        //this.type = type;
+    }
+
+    public User(string name, int age)
+    {
+        this.Name = name;
+        this.age = age;
+        //this.type = type;
+    }
+
+    public string Type
+    {
+        get => type;
+        init
+        {
+            type = value;
+        }
+    }
+
+    //public string Type
+    //{
+    //    get => type;
+    //}
+
+
+
+    public required string Name { set; get; } = "Anonim";
+    //{
+    //    get => name;
+    //    set => name = value;
+    //}
+
+    public int Age
+    {
+        get => age;
+        set 
+        { 
+            if(age > 0 && age < 100)
+                age = value; 
+        }
+    }
+
+
+    public void SetName(string name)
+    {
+        this.Name = name;
+    }
+
+    public string GetName()
+    {
+        return this.Name;
+    }
+
+    public void SetAge(int age)
+    {
+        if(age > 0 && age < 100)
+            this.age = age;
+    }
+    public int GetAge()
+    {
+        return this.age;
+    }
+
+}
+
+
+class Employee
+{
+    public string Name { set; get; }
+    public int Age { set; get; }
+}
